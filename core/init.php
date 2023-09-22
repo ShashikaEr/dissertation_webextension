2ec5296fe93f50e52e958edd65a6333eaa14176e
ShashikaEr <shashikaeranda@gmail.com>
2023-09-22 20:47:38 +0100

    Webextension_codes

--- core/init.php
+++ core/init.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *'); //allow cross origin requests from any domain
 include ('readURL.php');
 
 $read = new ViewURL();
